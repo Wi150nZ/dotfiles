680652b83fb506c7bb65d2b0ccc91b99411390e0
Wilson <wzhu7148@gmail.com>
2017-09-03 17:35:58 -0500

    Delete clementine_polybar.sh

--- scripts/clementine_polybar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# the following is a script for outputting music data from the clementine player
-
-# todo: conditional => if playing display title and artist
-title=$(qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata | grep title | cut -d " " -f2-)
