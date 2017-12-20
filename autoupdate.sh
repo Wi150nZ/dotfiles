682e816b75d4bd01bc1327a1cb652bf017c5fb1c
Wilson <wzhu7148@gmail.com>
2017-12-20 14:12:37 -0600

    Delete autoupdate.sh

--- autoupdate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# the following script will automatically update the repo with current dotfiles
-
-yes | cp -rf ~/.config/i3/config ~/Codespace/dotfiles/i3/config 
-yes | cp -rf ~/.config/neofetch/config ~/Codespace/dotfiles/neofetch/config
-yes | cp -rf ~/.config/polybar/* ~/Codespace/dotfiles/polybar/
-yes | cp -rf ~/.config/mpd/mpd.conf ~/Codespace/dotfiles/mpd.conf
